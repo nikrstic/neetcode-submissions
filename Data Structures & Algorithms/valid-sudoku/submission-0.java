8c2d4eab0e6c4b61e42c325a4a83dfe93d5de31d
nikrstic <62341762+nikrstic@users.noreply.github.com>
2026-05-17 12:31:35 +0200

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        ArrayList<HashSet<Character>> array = new ArrayList<HashSet<Character>>();
+        for(int i=0;i<27;i++){
+            array.add(new HashSet<Character>());
+        }
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                if(!(board[i][j] == '.')){
+                    // add gives false if element exists
+                    if(!array.get(j).add(board[i][j])){
+                        return false;
+                    }
+                    if(!array.get(9*1+i).add(board[i][j])){
+                        return false;
+                    }
+                    if(!array.get(9*2+(i/3)*3 +j/3).add(board[i][j])){
+                        return false;
+                    }
+                }
+            }   
+        }
+        return true;
+    }
+}
